c1f58bd25e96d29809eef19e66700d38cefff99d
Martsynyuk <terr83@gmail.com>
2017-01-04 21:32:19 +0200

    pdo conection

--- MVC/Controllers/Users.php
+++ MVC/Controllers/Users.php
@@ -3,6 +3,7 @@
 namespace MVC\Controllers;
 
 use System\Dispatcher;
+use PDO;
 
 /**
  * Class Users
@@ -16,7 +17,25 @@ class Users
      */
     public function loginAction()
     {
-        Dispatcher::getInstance()->display('users/login');
+    	try {
+    		$dsn = 'mysql:host=127.0.0.1;dbname=mysql;charset=utf8';
+    		$pdo = new PDO($dsn, 'root', '');
+    	} catch (PDOException $e) {
+    		die($e->getMessage());
+    	}
+    	
+    	$file = APP_ROOT . 'config/database/version_1.sql';
+    	
+    	if( file_exists($file) == true ) {
+    		$file = implode(' ', file( $file ));
+    		$patern[0] = '/(\/\*.*)/' ;
+    		$query = trim(preg_replace($patern, '', $file));
+    		
+    		$query = $pdo->prepare($query);
+    		$query->execute();
+    	}
+    	    	 
+        //Dispatcher::getInstance()->display('users/login');
     }
 
     /**
--- System/autoloader.php
+++ System/autoloader.php
@@ -1,6 +1,9 @@
 <?php
 
 spl_autoload_register(function ($className) {
-    $file = str_replace('\\', '/', $className) . '.php';
-    include_once $file;
+	$file = str_replace('\\', '/', $className) . '.php';
+	
+	if( file_exists($file) == true ) {
+		include_once $file;
+	}
 });
