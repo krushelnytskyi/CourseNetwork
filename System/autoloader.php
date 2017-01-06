a61b85ce644a4810e9a7b2c1d75bbfc49a402da1
Це мій компік <  >
2017-01-06 13:30:10 +0200

    manin

--- System/autoloader.php
+++ System/autoloader.php
@@ -1,6 +1,9 @@
 <?php
 
 spl_autoload_register(function ($className) {
+    var_dump($className);
     $file = str_replace('\\', '/', $className) . '.php';
     include_once $file;
 });
+
+// googlr
