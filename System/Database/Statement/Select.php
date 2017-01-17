2f6fd5033edfa80e7649925e2dd7ee633243b5d1
Martsynyuk <terr83@gmail.com>
2017-01-17 10:53:26 +0200

    create Select class

--- System/Database/Statement/Select.php
+++ System/Database/Statement/Select.php
@@ -13,21 +13,135 @@ class Select extends Statement
     protected $columns = '*';
 
     /**
-     * @param $columns
-     * @return $this
+     * @var string
      */
-    public function columns($columns)
+    protected $where = '';
+
+    /**
+     * @var string
+     */
+    protected $orderBy = '';
+
+    /**
+     * @var array
+     */
+    private $delimiters = ['=', '>', '<', '>=', '<=', '!='];
+
+    /**
+     * @param string|array $columns
+     * @return object $this
+     */
+    public function columns($columns = '*')
     {
-        $this->columns = $columns;
+        if (true == is_array($columns)) {
+            $this->columns = implode(',', $columns);
+        }
+
         return $this;
     }
 
-    public function where($criteria)
+    /**
+     * @param string $field
+     * @param string $delimiter
+     * @param string $value
+     * @return object $this
+     */
+    public function where($field, $delimiter, $value, $additionalCondition = '')
     {
+        if (false == in_array($delimiter, $this->delimiters)) {
+            return $this;
+        }
+
+        $this->where .= $additionalCondition . $field . $delimiter . $value;
+        return $this;
     }
 
+    /**
+     * @param string $field
+     * @param string $delimiter
+     * @param string $value
+     * @return objeect $this
+     */
+    public function andWhere($field, $delimiter, $value)
+    {
+        if ('' === $this->where) {
+            return $this;
+        }
+
+        $this->where($field, $delimiter, $value, ' AND');
+    }
+
+    /**
+     * @param string $field
+     * @param string $delimiter
+     * @param string $value
+     * @return object $this
+     */
+    public function orWhere($field, $delimiter, $value)
+    {
+        if ('' === $this->where) {
+            return $this;
+        }
+
+        $this->where($field, $delimiter, $value, ' OR');
+    }
+
+    /**
+     * @param string $field
+     * @param string $sort
+     * @return object $this
+     */
+    public function orderBy($field, $sort = 'ASC')
+    {
+        $this->orderBy = $field . ' ' . $sort;
+        return $this;
+    }
+
+    /**
+     * @param int $start
+     * @param int $finish
+     * @return object $this
+     */
+    public function limit($start = 0, $finish = 0)
+    {
+        if ((int)($start) == 0 && (int)($finish) == 0) {
+            return $this;
+        }
+
+        $this->limit = $start . ',' . $finish;
+        return $this;
+    }
+
+    /**
+     * @param int $offset
+     * @return object $this
+     */
+    public function offset($offset = 0)
+    {
+        if ((int)($offset) == 0) {
+            return $this;
+        }
+
+        $this->offset = $offset;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
     public function execute()
     {
+        if (false == $this->connection->getLink()) {
+            return null;
+        }
+
+        $sql = 'SELECT ' . $this->columns .
+                ' FROM ' . $this->table .
+                ' WHERE ' . $this->where .
+                ' ORDER BY ' . $this->orderBy .
+                ' OFFSEt ' . $this->offset . ';';
+
+        return $this->connection->getLink()->query($sql);
     }
 
 }
