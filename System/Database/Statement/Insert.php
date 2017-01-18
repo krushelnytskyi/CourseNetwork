c82078258f85ea8ac676daa772aee580ad34ae99
vitalii.krushelnytskyi <vitalii.krushelnytskyi@gmail.com>
2017-01-18 13:19:55 +0200

    Insert prepared for development. Stage 2

--- System/Database/Statement/Insert.php
+++ System/Database/Statement/Insert.php
@@ -7,7 +7,23 @@ use System\Database\Statement;
 class Insert extends Statement
 {
 
+    /**
+     * @var array
+     */
+    protected $columns;
+
+    /**
+     * @var array
+     */
     protected $values;
+
+    /**
+     * @param $columns
+     */
+    public function columns($columns)
+    {
+        $this->columns = $columns;
+    }
 
     /**
      * @param $values
@@ -15,10 +31,19 @@ class Insert extends Statement
      */
     public function values($values)
     {
-        $this->values = $values;
+        if (array_values($values) === $values) {
+            $this->values = $values;
+        } else {
+            $this->columns = array_keys($values);
+            $this->values = array_values($values);
+        }
+
         return $this;
     }
 
+    /**
+     * @return int Last inserted ID
+     */
     public function execute()
     {
         $this->values;
