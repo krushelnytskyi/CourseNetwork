c7edb62be9ef9699d26a95803de2207b96111748
drakoshafromzimbabve <bluzniuknataliazin@rambler.ru>
2017-01-25 15:05:43 +0200

    Update Delete.php

--- System/Database/Statement/Delete.php
+++ System/Database/Statement/Delete.php
@@ -1,104 +1,63 @@
 <?php
 
 namespace System\Database\Statement;
+
 use System\Database\Statement;
 
+/**
+ * Class Delete
+ * @package System\Database\Statement
+ */
 class Delete extends Statement
 {
+    
     /**
      * @var string
      */
-	 
-    public $where = '';
+    public $orderByLimit = '';
+	
 
-    public $orderbylimit = '';
-	
-	
-	
     /**
-     * it forms one of such expressions - WHERE, OR or AND
-     * depending on requirement and returns expression
+     * @param $orderBy
+     * @param $limit
+     * @return $this
      */
-    public function where($expression, $variable, $delimiter, $value)
+    public function orderByLimit ($orderBy, $limit)
     {
-		
-		if ($expression == 'WHERE' && $this->where === '') {
-			
-			
-            $this->where = ' ' . $expression . ' ' . $variable . $delimiter . $value;
-			
-			
-			} else if ($this->where !== '') {
-				
-			
-            $this->where = $this->where . $expression . $variable . $delimiter . $value;
-        }
-
-        return $this;
-    }
-
-
-	
-	
-	/**
-     * it determines some cases when expressions ORDER BY
-     * and LIMIT are need together - and returns expression
-     */
-    public function orderByLimit ($orderBy, $limit)  
-	{
-		
-
         if (($orderBy != '')&&($limit != 0)&&($orderBy != null)&&($limit != null)) {
-			
-			
             $this->orderByLimit = ' ORDER BY ' . $orderBy . ' LIMIT ' . $limit;
 			
-			
         } else {
-			
-            $this->orderByLimit = '';
-			
+            $this->orderByLimit = null;
         }
-		
 
         return $this;
     }
-	
-	
 
     /**
      * DELETE FROM table_name
      * DELETE FROM table_name WHERE id = 2 AND email = 'email@com'
      * DELETE
-     *
      * @return int
      */
-    public function execute()
+	public function execute()
     {
-		
-        $sql = 'DELETE FROM ' . $this->table;
-		
+        if (false == $this->connection->getLink()) {
+            return null;
+        }
 
-        if ($this->where !== '') {
-			
-			
-            $sql .= ' ' . $this->where;
-			
+        $sql = 'DELETE  FROM ' . $this->table;
+
+        if (null !== $this->where) {
+            $sql .= ' WHERE ' . $this->where;
+        }
+
+        if (null !== $this->orderByLimit) {
+            $sql .= $this->orderByLimit;
         }
 
         $result = $this->connection->getLink()->query($sql);
-		
-
-        if ($result !== false) {
-			
-			
-            return $this->connection->getLink()->affected_rows;
-			
-        }
-
-		
-        return false;
-
+        
     }
 	
 }
